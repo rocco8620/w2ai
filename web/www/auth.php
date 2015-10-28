7a92e00db01a2d098e1e9b67c8b753d800a56eee
rocco8620 <rocco8620@gmail.com>
2015-10-28 15:53:18 +0100

    aggiornamenti 20/10/15

--- web/www/auth.php
+++ web/www/auth.php
@@ -9,6 +9,9 @@ $pass = "admin";
 
 /* TODO: codice per determinare se l'utente è remoto o locale basandosi sull'ip */
 
+if (substr($_SERVER['REMOTE_ADDR'], 0, 8) == "192.168.") $remoto = false;
+else $remoto = true;
+
 include("../db_connect.txt"); // $conn : puntatore alla connessione
 
 $stmt = mysqli_prepare($conn, "SELECT id,attivo,privs,esterno,ip FROM utenti WHERE username = ? AND password = ?");
@@ -21,24 +24,46 @@ mysqli_stmt_bind_result($stmt, $id, $attivo, $privs, $esterno, $ip);
 
 mysqli_stmt_fetch($stmt);
 
-function givePrivs() {
-$_SESSION['ipArduino'] = $ip;
+function giveAdminPrivs() {
 $_SESSION['logged'] = true;
-$_SESSION['admin'] = $privs;
+$_SESSION['admin'] = true;
 }
+
+function giveUserPrivs() {
+	$_SESSION['ipArduino'] = $ip;
+	$_SESSION['logged'] = true;
+	$_SESSION['admin'] = false;
+}
+
 
 if ($id == null) echo "0|0|0|0"; // login ok | utente attivo | accessibile esterno | privilegi admin 
 else {
 	if ($attivo == true) {
-		if ($esterno == true) { echo "1|1|1|".$privs; givePrivs(); }
+		if ($esterno == true) {
+			if ($privs == true) {
+				echo "1|1|1|1";
+				giveAdminPrivs();
+			} else {
+				echo "1|1|1|0";
+				giveUserPrivs();
+			}
+		}
 		else {		
 			if ($remoto == true) echo "1|1|0|0";
-			else {echo  "1|1|1|0".$privs; givePrivs(); }
+			else {
+				if ($privs == true) {
+					echo "1|1|1|1";
+					giveAdminPrivs();
+				} else {
+					echo "1|1|1|0";
+					giveUserPrivs();
+				}
+			}
 		}
 	} else echo "1|0|0|0";
 }
 
-echo $id."|".$attivo."|".$privs."|".$esterno; /* TODO: Rimuovere questa riga quando finito*/
+// echo $id."|".$attivo."|".$privs."|".$esterno; /* TODO: Rimuovere questa riga quando finito*/
 
 mysqli_stmt_close($stmt);
 
