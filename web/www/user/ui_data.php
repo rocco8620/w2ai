87b1c03bb4831e6a6995db2373fb81b5ede6cad7
rocco8620 <rocco8620@gmail.com>
2015-11-03 13:31:08 +0100

    agg 03/11/15 (2)

--- web/www/user/ui_data.php
+++ web/www/user/ui_data.php
@@ -4,4 +4,5 @@ include("../includes/verify_auth.php");
 verify_user();
 include("../includes/db_connect.php"); // $conn : puntatore alla connessione
 /* TODO: inserire connessione al database */
+echo "<user_ui>";
 ?>
