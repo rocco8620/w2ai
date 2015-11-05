c8020c1283af40bb5fccf7894b4e53dbf82a52e7
rocco8620 <rocco8620@gmail.com>
2015-11-05 20:50:54 +0100

    agg 05/11/15 (2)

--- web/www/includes/widget_config.php
+++ web/www/includes/widget_config.php
@@ -1,8 +1,24 @@
 <?php
 
-    function encodeWidgetTemperaturaV1($nome, $indice, $max_temp, $min_temp, $freq_agg) {
-        $array = array('nome' => $nome, 'indice' => $indice, 'max_temp' => $max_temp, 'min_temp' => $min_temp, 'freq_agg' => $freq_agg);
-        return $array;
-    }
+function encodeUserUi($user_data, $widget_usati) {
+	$array = array($user_data, $widget_usati);
+}
+
+function encodeUserData($nome, $ip) {
+	$array = array($nome, $ip);
+	return $array;
+}
+
+function encodeWidgetTemperaturaV1($nome, $indice, $freq_agg) {
+	$array = array(1, $nome, $indice, $freq_agg); // array da 4 elementi
+	return $array;
+}
+
+function encodeWidgetTemperaturaV2($nome, $indice, $max_temp, $min_temp, $freq_agg, $max_temp_action, $min_temp_action) {
+	$array = array(2, $nome, $indice, $max_temp, $min_temp, $freq_agg, $max_temp_action, $min_temp_action);
+	return $array;
+}
+    
 
 ?>
+
