c5c285917c6827b89cc2e23a8d37ae00ff3bb6df
rocco8620 <rocco8620@gmail.com>
2015-11-11 13:17:12 +0100

    agg 10/11/15

--- web/www/admin/ui_data.php
+++ web/www/admin/ui_data.php
@@ -4,4 +4,20 @@ include("../includes/functions.php");
 verify_admin();
 include("../includes/db_connect.php"); // $conn : puntatore alla connessione
 /* TODO: inserire connessione al database */
+
+$own = 1;
+
+$result = mysqli_query($conn, "SELECT * FROM users");
+
+$result = mysqli_query($conn, "SELECT username,ip FROM utenti WHERE own = ".$own);
+$rows = mysqli_fetch_array($result, MYSQLI_NUM);
+
+/* TODO: ROBA DA FINIRE */
+
+echo json_encode(encodeUi(encodeUserData($rows[0], $rows[1]), $widgets_list));
+
+mysqli_free_result($result);
+
+mysqli_close($conn);
+
 ?>
